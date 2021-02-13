00b608eee225ee168bf91847fd3e09dab3b3c3ae
williamren97 <williamren97@gmail.com>
2021-02-13 23:12:51 +0800

    1

---
+++ src/com/rw/test4Interview/Demo02.java
@@ -0,0 +1,18 @@
+package com.rw.test4Interview;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author WilliamRen
+ * @version
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        int[] arrays = {1, 2, 4, 3, 5};
+        for (int array : arrays) {
+            System.out.println(array);
+        }
+    }
+    Arrays
+}
---
+++ src/com/rw/test4Interview/Main.java
@@ -0,0 +1,14 @@
+package com.rw.test4Interview;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World")
+        ;
+
+    }
+}
+
+
+
